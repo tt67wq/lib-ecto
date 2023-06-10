b5aac2be2703f23d299f6a0146fd53979edb69ac
tt67wq <tt67wq@aliyun.com>
2023-06-10 21:40:09 +0800

    feat: add ex_doc

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule LibEcto.MixProject do
     [
       {:jason, "~> 1.4"},
       {:ecto_sql, "~> 3.10"},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
