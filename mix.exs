cfe09b38b0147127ac98452eeb7903cee270a41e
tt67wq <tt67wq@aliyun.com>
2023-06-10 21:39:21 +0800

    feat: for hexpm

--- mix.exs
+++ mix.exs
@@ -1,13 +1,22 @@
 defmodule LibEcto.MixProject do
   use Mix.Project
 
+  @name "lib_ecto"
+  @version "0.1.0"
+  @repo_url "https://github.com/tt67wq/lib-ecto"
+  @description "A library to make Ecto easier to use"
+
   def project do
     [
       app: :lib_ecto,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: @repo_url,
+      name: @name,
+      package: package(),
+      description: @description
     ]
   end
 
@@ -23,9 +32,17 @@ defmodule LibEcto.MixProject do
     [
       {:jason, "~> 1.4"},
       {:ecto_sql, "~> 3.10"},
-      {:ecto_sqlite3, "~> 0.10", only: :test, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @repo_url
+      }
     ]
   end
 end
