36d8be136ef47f7d9692d6ba9d1dc2bcd65c65bd
wanqiang <wanqiang@shizhuang-inc.com>
2024-01-17 15:37:26 +0800

    Update version number to 0.3.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LibEcto.MixProject do
   use Mix.Project
 
   @name "lib_ecto"
-  @version "0.3.1"
+  @version "0.3.2"
   @repo_url "https://github.com/tt67wq/lib-ecto"
   @description "A library to make Ecto easier to use"
 
